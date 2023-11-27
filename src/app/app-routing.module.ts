84bfc35e6afd2e0d2792c48964ae171eb4589f88
bastianmadrid <ba.madridb@duocuc.cl>
2023-11-27 02:00:17 -0300

    creacion de la caprtea servicios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NoingresadoGuard } from './noingresado.guard';
+import { IngresadoGuard } from './ingresado.guard';
 
 const routes: Routes = [
   {
@@ -9,27 +11,27 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule), canActivate:[NoingresadoGuard]
   },
   {
     path: 'base',
-    loadChildren: () => import('./pages/base/base.module').then( m => m.BasePageModule)
+    loadChildren: () => import('./pages/base/base.module').then( m => m.BasePageModule), canActivate:[IngresadoGuard]
   },
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule), canActivate:[NoingresadoGuard]
   },
   {
     path: 'qrreader',
-    loadChildren: () => import('./pages/qrreader/qrreader.module').then( m => m.QrreaderPageModule)
+    loadChildren: () => import('./pages/qrreader/qrreader.module').then( m => m.QrreaderPageModule),canActivate:[IngresadoGuard]
   },
   {
     path: 'recuperar-contra',
-    loadChildren: () => import('./pages/recuperar-contra/recuperar-contra.module').then( m => m.RecuperarContraPageModule)
+    loadChildren: () => import('./pages/recuperar-contra/recuperar-contra.module').then( m => m.RecuperarContraPageModule), canActivate:[NoingresadoGuard]
   },
   {
     path: 'pregunta',
-    loadChildren: () => import('./pages/pregunta/pregunta.module').then( m => m.PreguntaPageModule)
+    loadChildren: () => import('./pages/pregunta/pregunta.module').then( m => m.PreguntaPageModule), canActivate:[NoingresadoGuard]
   }
 ];
 
